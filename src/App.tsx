2970d2afe9f70c21b73de0892692b49ac7a332ee
yacine37th <y.titouah@esi-sba.dz>
2023-11-06 20:43:45 +0100

    nework state added

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,51 @@ import Home from "./home/Home";
 import NormalPackUsers from "./services/NormalPackUsers";
 import GoldenPackUsers from "./services/GoldenPackUsers";
 import DiamandPackUsers from "./services/DiamandPackUsers";
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useState } from "react";
+// import NetworkConnection from "./services/NetworkConnection";
 
 const App: React.FC = () => {
+  const [status, setStatus] = useState<boolean>(() => {
+    if (navigator.onLine) {
+      console.log("Connected to network.");
+      return true;
+    } else {
+      return false;
+    }
+  });
+  useEffect(() => {
+    window.ononline = (e) => {
+      console.log("Connected to network.");
+      setStatus(true);
+      console.log('====================================');
+      console.log(e);
+      console.log('====================================');
+    };
+    window.onoffline = (e) => {
+      console.log("Network connection lost.");
+      setStatus(false);
+      console.log(e);
+      console.log('====================================');
+    };
+  }, [status]);
   return (
     <>
       {/* <button onClick={showToast}>Submit</button> */}
       <ToastContainer />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/normalPackUsers" element={<NormalPackUsers />} />
-        <Route path="/goldenPackUsers" element={<GoldenPackUsers />} />
-        <Route path="/diamandPackUsers" element={<DiamandPackUsers />} />
-      </Routes>
+      {/* {status ? ( */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/normalPackUsers" element={<NormalPackUsers />} />
+          <Route path="/goldenPackUsers" element={<GoldenPackUsers />} />
+          <Route path="/diamandPackUsers" element={<DiamandPackUsers />} />
+        </Routes>
+      {/*  ) : (
+         <Routes>
+           <Route path="/" element={<NetworkConnection />} />
+         </Routes>
+       )} */}
     </>
   );
 };
---
+++ src/services/NetworkConnection.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+function NetworkConnection() {
+  return (
+    <div className='flex justify-center items-center flex-1 h-screen w-screen'>
+        <div className='text-3xl text-red-500'>
+            No Connexion
+        </div>
+    </div>
+  )
+}
+
+export default NetworkConnection
